2ad9ffe731b4b1be8cd5445154a22ff817a6db95
sushigadaisuki <osushi>
2024-12-10 07:46:51 +0900

    append connect

--- dlsite_seiheki_analyzer.user.js
+++ dlsite_seiheki_analyzer.user.js
@@ -8,7 +8,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @require      https://cdnjs.cloudflare.com/ajax/libs/pako/2.1.0/pako.min.js
 // @grant        GM_xmlhttpRequest
-// @connect      *
+// @connect      github.com
+// @connect      raw.githubusercontent.com
 // ==/UserScript==
 
 (async function () {
